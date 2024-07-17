b45bdc5f7dca17efde7e4796fb88b4b03e1bf22b
Juan Sáez García <saez@oscala.es>
2024-07-17 12:30:09 +0200

    feat(InspectionMethod): prevent null exception in id getter

--- src/main/java/nl/rgs/kib/model/method/InspectionMethod.java
+++ src/main/java/nl/rgs/kib/model/method/InspectionMethod.java
@@ -14,6 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Data()
 @EqualsAndHashCode(callSuper = true)
@@ -48,7 +49,7 @@ public class InspectionMethod extends BaseObject {
     }
 
     public String getId() {
-        return id.toHexString();
+        return Optional.ofNullable(id).map(ObjectId::toHexString).orElse(null);
     }
 
     public void setId(String id) {
