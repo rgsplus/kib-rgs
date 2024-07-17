ead784751a3c2763c2146eda83470213f5e4e03e
Juan Sáez García <saez@oscala.es>
2024-07-17 12:30:53 +0200

    test(InspectionMethodService): added unit tests

--- src/test/java/nl/rgs/kib/service/InspectionMethodServiceImplTest.java
+++ src/test/java/nl/rgs/kib/service/InspectionMethodServiceImplTest.java
@@ -1,6 +1,9 @@
 package nl.rgs.kib.service;
 
 import nl.rgs.kib.model.method.InspectionMethod;
+import nl.rgs.kib.model.method.InspectionMethodCalculationMethod;
+import nl.rgs.kib.model.method.InspectionMethodInput;
+import nl.rgs.kib.model.method.dto.CreateInspectionMethod;
 import nl.rgs.kib.repository.InspectionMethodRepository;
 import nl.rgs.kib.service.impl.InspectionMethodServiceImpl;
 import org.bson.types.ObjectId;
@@ -29,6 +32,17 @@ public class InspectionMethodServiceImplTest {
     private InspectionMethodServiceImpl inspectionMethodService;
 
     @Test
+    public void count_ReturnsCount() {
+        long expected = 5;
+        when(inspectionMethodRepository.count()).thenReturn(expected);
+
+        long result = inspectionMethodService.count();
+
+        assertEquals(expected, result);
+        verify(inspectionMethodRepository).count();
+    }
+
+    @Test
     public void findAll_ReturnsAllInspectionMethods() {
         InspectionMethod method1 = new InspectionMethod();
         InspectionMethod method2 = new InspectionMethod();
@@ -54,8 +68,106 @@ public class InspectionMethodServiceImplTest {
         verify(inspectionMethodRepository).findById(id);
     }
 
-    //TODO: Implement tests for count method
-    //TODO: Implement tests for create method
-    //TODO: Implement tests for update method
-    //TODO: Implement tests for delete method
+    @Test
+    public void findById_ReturnsEmptyBecauseMethodNotFound() {
+        ObjectId id = new ObjectId();
+        when(inspectionMethodRepository.findById(id)).thenReturn(Optional.empty());
+
+        Optional<InspectionMethod> result = inspectionMethodService.findById(id);
+
+        assertTrue(result.isEmpty());
+        verify(inspectionMethodRepository).findById(id);
+    }
+
+    @Test
+    public void create_CreatesInspectionMethod() {
+        CreateInspectionMethod createInspectionMethod = new CreateInspectionMethod(
+                "name",
+                InspectionMethodInput.PERCENTAGE,
+                InspectionMethodCalculationMethod.NEN2767,
+                List.of()
+        );
+
+        InspectionMethod expected = new InspectionMethod();
+        expected.setName(createInspectionMethod.name());
+        expected.setInput(createInspectionMethod.input());
+        expected.setCalculationMethod(createInspectionMethod.calculationMethod());
+        expected.setStages(InspectionMethod.sortStages(createInspectionMethod.stages()));
+
+        when(inspectionMethodRepository.save(expected)).thenReturn(expected);
+
+        InspectionMethod result = inspectionMethodService.create(createInspectionMethod);
+
+        assertEquals(expected, result);
+        verify(inspectionMethodRepository).save(expected);
+    }
+
+    @Test
+    public void update_UpdatesInspectionMethod() {
+        InspectionMethod existingMethod = new InspectionMethod();
+        existingMethod.setId(new ObjectId().toHexString());
+        existingMethod.setName("existingName");
+        existingMethod.setInput(InspectionMethodInput.PERCENTAGE);
+        existingMethod.setCalculationMethod(InspectionMethodCalculationMethod.NEN2767);
+        existingMethod.setStages(List.of());
+
+        InspectionMethod updatedMethod = new InspectionMethod();
+        updatedMethod.setId(existingMethod.getId());
+        updatedMethod.setName("updatedName");
+        updatedMethod.setInput(InspectionMethodInput.PERCENTAGE);
+        updatedMethod.setCalculationMethod(InspectionMethodCalculationMethod.NEN2767);
+        updatedMethod.setStages(List.of());
+
+        when(inspectionMethodRepository.findById(new ObjectId(existingMethod.getId()))).thenReturn(Optional.of(existingMethod));
+        when(inspectionMethodRepository.save(existingMethod)).thenReturn(existingMethod);
+
+        Optional<InspectionMethod> result = inspectionMethodService.update(updatedMethod);
+
+        assertTrue(result.isPresent());
+        assertEquals(existingMethod, result.get());
+        verify(inspectionMethodRepository).findById(new ObjectId(existingMethod.getId()));
+        verify(inspectionMethodRepository).save(existingMethod);
+    }
+
+    @Test
+    public void update_NotUpdatesBecauseMethodNotFound() {
+        InspectionMethod updatedMethod = new InspectionMethod();
+        updatedMethod.setId(new ObjectId().toHexString());
+        updatedMethod.setName("updatedName");
+        updatedMethod.setInput(InspectionMethodInput.PERCENTAGE);
+        updatedMethod.setCalculationMethod(InspectionMethodCalculationMethod.NEN2767);
+        updatedMethod.setStages(List.of());
+
+        when(inspectionMethodRepository.findById(new ObjectId(updatedMethod.getId()))).thenReturn(Optional.empty());
+
+        Optional<InspectionMethod> result = inspectionMethodService.update(updatedMethod);
+
+        assertTrue(result.isEmpty());
+        verify(inspectionMethodRepository).findById(new ObjectId(updatedMethod.getId()));
+    }
+
+    @Test
+    public void deleteById_DeletesInspectionMethod() {
+        ObjectId id = new ObjectId();
+        InspectionMethod method = new InspectionMethod();
+        when(inspectionMethodRepository.findById(id)).thenReturn(Optional.of(method));
+
+        Optional<InspectionMethod> result = inspectionMethodService.deleteById(id);
+
+        assertTrue(result.isPresent());
+        assertEquals(method, result.get());
+        verify(inspectionMethodRepository).findById(id);
+        verify(inspectionMethodRepository).deleteById(id);
+    }
+
+    @Test
+    public void deleteById_NotDeletesBecauseMethodNotFound() {
+        ObjectId id = new ObjectId();
+        when(inspectionMethodRepository.findById(id)).thenReturn(Optional.empty());
+
+        Optional<InspectionMethod> result = inspectionMethodService.deleteById(id);
+
+        assertTrue(result.isEmpty());
+        verify(inspectionMethodRepository).findById(id);
+    }
 }
