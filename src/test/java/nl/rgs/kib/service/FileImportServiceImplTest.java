11cc73823679cfe0234b6eebbf73871d00d4606b
Juan Sáez García <saez@oscala.es>
2024-09-04 15:10:04 +0200

    test(FileImportService):
    added failure test

--- src/test/java/nl/rgs/kib/service/FileImportServiceImplTest.java
+++ src/test/java/nl/rgs/kib/service/FileImportServiceImplTest.java
@@ -1,5 +1,7 @@
 package nl.rgs.kib.service;
 
+import nl.rgs.kib.model.list.InspectionList;
+import nl.rgs.kib.shared.models.ImportResult;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +12,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @ActiveProfiles("production")
 public class FileImportServiceImplTest {
@@ -18,9 +22,30 @@ public class FileImportServiceImplTest {
     private FileImportService fileImportService;
 
     @Test
-    public void testImportFile() throws IOException {
+    public void testImportFileSuccess() throws IOException {
         Path path = Paths.get("src/test/resources/kib-blueprint.xlsx");
         byte[] file = Files.readAllBytes(path);
-        fileImportService.importExcelBluePrint(file, "Test blueprint");
+        String fileName = "Test blueprint";
+
+        ImportResult<InspectionList> importResult = fileImportService.importExcelBluePrint(file, fileName);
+
+        assertNotNull(importResult);
+        assertNotNull(importResult.getResult());
+        assertNotNull(importResult.getResult().getId());
+        assertEquals(fileName, importResult.getResult().getName());
+        assertEquals(0, importResult.getErrors().size());
+    }
+
+    @Test
+    public void testImportFileFailure() throws IOException {
+        Path path = Paths.get("src/test/resources/kib-blueprint-with-errors.xlsx");
+        byte[] file = Files.readAllBytes(path);
+        String fileName = "Test blueprint with errors";
+
+        ImportResult<InspectionList> importResult = fileImportService.importExcelBluePrint(file, fileName);
+
+        assertNotNull(importResult);
+        assertNull(importResult.getResult());
+        assertEquals(6, importResult.getErrors().size());
     }
 }
