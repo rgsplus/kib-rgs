4bf78ed281ea2b7756298b6638bcface94740f45
Juan Sáez García <saez@oscala.es>
2024-10-14 13:06:07 +0200

    chore: remove comment

--- src/main/java/nl/rgs/kib/model/user/dto/CreateUser.java
+++ src/main/java/nl/rgs/kib/model/user/dto/CreateUser.java
@@ -5,8 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import nl.rgs.kib.model.user.UserRole;
 
-//TODO: Define Create User constraints and validations for the properties
-
 /**
  * CreateUser
  * <p>
